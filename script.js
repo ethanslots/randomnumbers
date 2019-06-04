1fbeda2c9ff9eec59ff1e995cb9cd9b5f6fd6978
ethanslots <47529341+ethanslots@users.noreply.github.com>
2019-06-04 09:04:45 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+// Variables
+
+var x = 0, y = '';
+var message = ['Ah ', 'h', 'i', 's', 't', ', something went wrong.'];
+
+// Functions
+
+function go() {
+  x = Math.floor(Math.random() * 101);
+  if (x !== 55) {
+    document.write('Drumroll please...');
+    setTimeout(writing(x), 1500);
+  } else {
+    document.write('Drumroll please...');
+    setTimeout(writing(message[0] + message[3] + message[1] + message[2] + message[4] + message[5]), 1500);
+}
+
+function writing(y) {
+  document.write(y);
+}
